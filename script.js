08da9a81bf8180e7cf9d883164a5ed123158ee79
Mattijs Kersten <kersten.m.1@pg.com>
2015-12-04 15:19:27 +0100

    Switch to Angular!

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-var name="Mattijs";
-//alert('Welcome ' + name + "!");
+var app = angular.module('store', []);
+
+app.controller('StoreController', function() {
+  this.product = [{
+    name: 'Downton Abbey BOGOF',
+    image: 'wine1.jpg'
+  }, {
+    name: 'Chateau Rouge',
+    image: 'wine2.jpg'
+  }, {
+    name: 'Green Hills',
+    image: 'wine3.jpg'
+  }, ];
+});
