63ccd254e4837b5e3cd24930467a095a36b3a1b3
Mattijs Kersten <kersten.m.1@pg.com>
2015-12-04 15:58:30 +0100

    add pricing and totals tracking

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 var app = angular.module('store', []);
 
 app.controller('StoreController', function() {
-  this.products = [{
+
+	var store = this;
+
+	store.products = [{
     name: 'Downton Abbey BOGOF',
-    image: 'wine1.jpg'
+    image: 'wine1.jpg',
+    price: 15.59,
+    quantity: 0
   }, {
     name: 'Chateau Rouge',
-    image: 'wine2.jpg'
+    image: 'wine2.jpg',
+    price: 10.99,
+    quantity: 0
   }, {
     name: 'Green Hills',
-    image: 'wine3.jpg'
+    image: 'wine3.jpg',
+    price: 5.49,
+    quantity: 0
   }, ];
+
+  this.getTotal = function() {
+    total = 0;
+    for(i = 0; i < store.products.length; i++) {
+      total = total + store.products[i].price * store.products[i].quantity;
+    }
+    return total;
+  }
+
 });
