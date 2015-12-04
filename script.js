ed527956f811a6b4563ad69d6d51a73e56397ec0
Mattijs Kersten <kersten.m.1@pg.com>
2015-12-04 16:13:01 +0100

    move products to separate json file

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
 var app = angular.module('store', []);
 
-app.controller('StoreController', function() {
+app.controller('StoreController', ['$http', function($http) {
 
-	var store = this;
+  var store = this;
 
-	store.products = [{
-    name: 'Downton Abbey BOGOF',
-    image: 'wine1.jpg',
-    price: 15.59,
-    quantity: 0
-  }, {
-    name: 'Chateau Rouge',
-    image: 'wine2.jpg',
-    price: 10.99,
-    quantity: 0
-  }, {
-    name: 'Green Hills',
-    image: 'wine3.jpg',
-    price: 5.49,
-    quantity: 0
-  }, ];
+  store.products = [];
+
+  $http.get('products.json').success(
+    function(data) {
+      store.products = data;
+    }
+  );
 
   this.getTotal = function() {
     total = 0;
-    for(i = 0; i < store.products.length; i++) {
+    for (i = 0; i < store.products.length; i++) {
       total = total + store.products[i].price * store.products[i].quantity;
     }
     return total;
   }
 
-});
+}]);
